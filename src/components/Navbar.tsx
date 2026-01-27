784f92e5fe490fe19a1b82fa86474d19f7884413
Anastasiia Ustin <2anastasiia@mail.ru>
2026-01-27 18:53:38 +0200

    init commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Button, Container, Nav, Navbar as NavbarBs} from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
+import { useShoppingCart } from '../context/ShoppingCartContext'
+
+function Navbar() {
+    const { openCart, cartQuantity } = useShoppingCart()
+    return (
+        <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
+            <Container>
+                <Nav className='me-auto'>
+                    <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+                    <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
+                    <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
+                </Nav>
+                {cartQuantity > 0 && <Button onClick={openCart} style={{position: 'relative'}}>cart
+                    <div style={{ color: 'white', width: '1.5rem', height: '1.5rem', position: 'absolute', bottom: 0, right: 0, transform: 'translate(25%, 25%)' }} className='rounded-circle bg-danger d-flex justify-content-center align-items-center'>{cartQuantity}</div>
+                </Button>}
+                
+            </Container>
+        </NavbarBs>
+    )
+}
+
+export default Navbar
